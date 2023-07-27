ebbb3afd67a436505eed8f17f49a9baf7be8cf05
ryansevidal <89810484+ryansevidal@users.noreply.github.com>
2023-07-26 20:44:51 -0700

    Skeleton Code

--- App.js
+++ App.js
@@ -1,39 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 export default function App() {
   return (
-    <><StatusBar barStyle="dark-content" hidden={false} backgroundColor="#00BCD4" transulucent={true} /><View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your Fitness Goal' />
-        <Button title='Add' />
-      </View>
-      <View>
-        <Text>List of goals...</Text>
-      </View>
-    </View></>
+    <StatusBar barStyle="dark-content" hidden={false} backgroundColor="#00BCD4" transulucent={true} />
+
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    marginTop: 100,
-    marginRight: 20,
-    marginLeft: 20,
-    
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '80%',
-    marginRight: 8,
-    padding: 8,
 
-  }
 });
 
 const bottomNavBar
