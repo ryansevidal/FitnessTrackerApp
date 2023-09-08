f9c1e95a23c7afea743825fc504d34b544d948d9
trishatong <trtong1@gmail.com>
2023-09-08 12:28:34 -0700

    added progress bar

--- DietScreen.js
+++ DietScreen.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Modal, TextInput, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Button, Modal, TextInput, FlatList, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { Bar } from 'react-native-progress';
 
 const DietScreen = () => {
   const [foodLog, setFoodLog] = useState([]);
   const [isModalVisible, setModalVisible] = useState(false);
   const [foodName, setFoodName] = useState('');
-  const [calories, setCalories] = useState('');
+  const [calories, setCalories] = useState(0);
+  const [maxCalories, setMaxCalories] = useState(0);
+  const [showMaxCaloriesModal, setShowMaxCaloriesModal] = useState(false);
 
   const toggleModal = () => {
     setModalVisible(!isModalVisible);
+  };
+
+  const toggleMaxCaloriesModal = () => {
+    setShowMaxCaloriesModal(!showMaxCaloriesModal);
+  };
+
+  const saveMaxCalories = () => {
+    if (!isNaN(maxCalories)) {
+      setMaxCalories(maxCalories);
+      toggleMaxCaloriesModal();
+    } else {
+      Alert.alert('Invalid Max Calories', 'Please enter a valid number for Max Calories.');
+    }
   };
 
   const addFoodEntry = () => {
@@ -17,16 +34,37 @@ const DietScreen = () => {
       setFoodName('');
       setCalories('');
       toggleModal();
+    } else {
+      Alert.alert('Invalid Entry', 'Please enter a valid food name and a valid number for Calories.');
     }
   };
 
   const totalCalories = foodLog.reduce((total, item) => total + parseFloat(item.calories), 0);
 
+  const progress = maxCalories > 0 ? totalCalories / maxCalories : 0;
+
   return (
     <View style={styles.container}>
       <View style={styles.headerWrapper}>
         <Text style={styles.headerText}>Diet Tracker</Text>
-        <Text style={styles.totalCalories}>Total Calories: {totalCalories}</Text>
+        <Text style={styles.totalCalories}>Total Calories: {totalCalories} / {maxCalories}</Text>
+        <Ionicons
+          name="cog-outline"
+          size={24}
+          color="white"
+          style={styles.maxCaloriesButton}
+          onPress={toggleMaxCaloriesModal}
+        />
+        <Bar
+          progress={progress} // Progress should be between 0 and 1
+          width={null} // Use null to allow automatic width adjustment
+          height={10} // Customize the height of the progress bar
+          borderColor="#a37cf4" // Customize the border color
+          unfilledColor="#121212" // Customize the unfilled part color
+          color="#a37cf4" // Customize the filled part color
+          borderRadius={0} // Customize the border radius
+          style={styles.progressBar}
+        />
       </View>
       <TouchableOpacity
         style={styles.textWrapper}
@@ -67,6 +105,22 @@ const DietScreen = () => {
           </View>
         </View>
       </Modal>
+      <Modal animationType="slide" transparent={true} visible={showMaxCaloriesModal}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalHeading}>Set Max Calories</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Max Calories"
+              value={maxCalories}
+              onChangeText={(text) => setMaxCalories(text)}
+              keyboardType="numeric"
+            />
+            <Button title="Save" onPress={saveMaxCalories} />
+            <Button title="Cancel" onPress={toggleMaxCaloriesModal} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
@@ -87,6 +141,16 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 32,
     fontWeight: 'bold',
+  },
+  maxCaloriesButton: {
+    position: 'absolute',
+    bottom: '60%',
+    left: '92%',
+    padding: 5,
+    borderRadius: 20,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   totalCalories: {
     color: '#ffffff',
@@ -143,6 +207,10 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     borderBottomWidth: 1,
+  },
+  progressBar: {
+    width: '100%',
+    marginTop: '4%',
   },
 });
 
